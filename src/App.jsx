bff59b8cc9ebb92408b96c597a3007040a710cc1
kola kolade <kolakolade@gmail.com>
2024-05-17 23:05:47 +0100

    Setup routing with react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import About from './containers/about/About';
+import Contact from './containers/contact/Contact';
+import Home from './containers/home/Home';
+import Portfolio from './containers/portfolio/Portfolio';
+
 function App() {
   return (
-    <>
-      <div>
-        <h1>Hello !</h1>
-      </div>
-    </>
+    <div className="App">
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/containers/home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>kolakolade.me</h1>
+    </div>
+  );
+};
+
+export default Home;
